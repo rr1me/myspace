0dc384ec598ffa46787b1ee98a6ce80f35b4f06b
rime <s1337228@yandex.ru>
2023-12-02 00:06:13 +0300

    hard marquee clean

--- src/app/components/molecules/HardMarquee/HardMarquee.tsx
+++ src/app/components/molecules/HardMarquee/HardMarquee.tsx
@@ -3,7 +3,6 @@
 import s from './HardMarquee.module.scss';
 import { useEffect, useRef, useState } from 'react';
 import SxSC from '@/app/components/atoms/SxSC/SxSC';
-import { delay } from '@/app/components/shared/utils';
 
 const HardMarquee = () => {
 	const firstRef = useRef<HTMLDivElement>(null);
@@ -20,51 +19,43 @@ const HardMarquee = () => {
 	});
 
 	useEffect(() => {
-		(async () => {
-			const length = first.child.props.children.length;
-			const part = 100/ length;
-			const intervalDelay = 300;
+		const length = first.child.props.children.length;
+		const part = 100/ length;
+		const intervalDelay = 300;
+		let initialized = false;
 
-			// for (let i = 0; i < length; i++) {
-			// 	setInnerTransform(x => x + part >= -100 ? -100 : x + part);
-			// 	if (i + 1 !== length)
-			// 		await delay(intervalDelay);
-			// }
-			let initialized = false;
+		const interval = setInterval(() => {
+			if (!initialized){
+				setInnerTransform(x => {
+					const sum = x + part;
+					if (sum >= -100){
+						initialized = true;
+						return -100;
+					}
+					return sum;
+				});
+				return;
+			}
 
-			const interval = setInterval(() => {
-				if (!initialized){
-					setInnerTransform(x => {
-						const sum = x + part;
-						if (sum >= -100){
-							initialized = true;
-							return -100;
-						}
-						return sum;
-					});
-					return;
+			setFirst(x => {
+				const currentTransform = Number(/\((.+)%/g.exec(x.transform)![1]);
+
+				if (currentTransform + part >= 100){
+					setSecond(() =>
+						({ child: x.child, transform: 'translateY(0%)' }));
+					return { child: getRandomElement(), transform: 'translateY(0%)' };
 				}
 
-				setFirst(x => {
-					const currentTransform = Number(/\((.+)%/g.exec(x.transform)![1]);
+				const transformValue = `translateY(${currentTransform + part}%)`;
+				setSecond((s) =>
+					({ ...s, transform: transformValue }));
+				return { ...x, transform: transformValue };
+			});
+		}, intervalDelay);
 
-					if (currentTransform + part >= 100){
-						setSecond(() =>
-							({ child: x.child, transform: 'translateY(0%)' }));
-						return { child: getRandomElement(), transform: 'translateY(0%)' };
-					}
-
-					const transformValue = `translateY(${currentTransform + part}%)`;
-					setSecond((s) =>
-						({ ...s, transform: transformValue }));
-					return { ...x, transform: transformValue };
-				});
-			}, intervalDelay);
-
-			return () => {
-				clearInterval(interval);
-			};
-		})();
+		return () => {
+			clearInterval(interval);
+		};
 	}, []);
 
 	return (
--- src/app/components/shared/utils.ts
+++ src/app/components/shared/utils.ts
@@ -1,6 +1,3 @@
 import { css, CSSObject, CSSProperties } from 'styled-components';
 
 export const applyStyles = (styles: CSSProperties) => css`${styles as CSSObject}`;
-
-export const delay = async (ms: number) =>
-	new Promise(r => setTimeout(r, ms));
