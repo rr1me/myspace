6191c2e931ae65cb5d9e73be61d54053defe07c7
rime <s1337228@yandex.ru>
2023-12-22 17:39:43 +0300

    marquee pre minHeight

--- src/app/components/molecules/Marquee/Marquee.tsx
+++ src/app/components/molecules/Marquee/Marquee.tsx
@@ -1,13 +1,34 @@
 'use client';
 
 import s from './Marquee.module.scss';
-import { ReactNode } from 'react';
+import { forwardRef, ReactNode, useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import SxSC from '@/app/components/atoms/SxSC/SxSC';
 import { CSSProperties } from 'styled-components';
 
 const Marquee = ({ children, horizontal, sx, duration = 30 }:
 	{children: ReactNode, horizontal?: boolean, sx?: CSSProperties, duration?: number}) => {
+
+	const wrapperRef = useRef<HTMLDivElement>(null);
+	const firstPreRef = useRef<HTMLPreElement>(null);
+	const secondPreRef = useRef<HTMLPreElement>(null);
+
+	useEffect(() => {
+		const
+			wrapper = wrapperRef.current,
+			firstPre = firstPreRef.current,
+			secondPre = secondPreRef.current;
+		if (!wrapper || !firstPre || !secondPre) return;
+
+		const preHeight = firstPre.getBoundingClientRect().height;
+		const wrapperHeight = wrapper.getBoundingClientRect().height;
+
+		if (preHeight >= wrapperHeight) return;
+		const height = wrapperHeight + 'px';
+		firstPre.style.height = height;
+		secondPre.style.height = height;
+
+	}, []);
 
 	const classNameWrapper = clsx({
 		[s.codeElement]: true,
@@ -21,25 +42,63 @@ const Marquee = ({ children, horizontal, sx, duration = 30 }:
 		[s.animHorizontal]: horizontal
 	});
 
-	const Code =
-		<SxSC as='pre' $sx={{
-			animationDuration: duration/2 + 's',
-			animationDelay: duration + 's'
-		}} className={classNamePre}>
-			<code>
-				{children}
-			</code>
-		</SxSC>;
+	// const Pre =
+	// 	<SxSC as='pre' $sx={{
+	// 		animationDuration: duration/2 + 's',
+	// 		animationDelay: duration + 's'
+	// 	}} className={classNamePre}>
+	// 		<code>
+	// 			{children}
+	// 		</code>
+	// 	</SxSC>;
 
 	return (
-		<SxSC $sx={{
-			animationDuration: duration + 's',
-			...sx
-		}} className={classNameWrapper}>
-			{Code}
-			{Code}
-		</SxSC>
+		<div className={s.marqueeWrapper} ref={wrapperRef}>
+			<SxSC $sx={{
+				animationDuration: duration + 's',
+				...sx
+			}} className={classNameWrapper}>
+				{/*{Pre}*/}
+				{/*{Pre}*/}
+				<Pre ref={firstPreRef} className={classNamePre} duration={duration}>
+					{children}
+				</Pre>
+				<Pre ref={secondPreRef} className={classNamePre} duration={duration}>
+					{children}
+				</Pre>
+			</SxSC>
+		</div>
 	);
 };
 
 export default Marquee;
+
+// const Pre = ({ children, className, duration }:
+// 	{children: ReactNode, className: string, duration: number}) => (
+// 	<SxSC as='pre' $sx={{
+// 		animationDuration: duration/2 + 's',
+// 		animationDelay: duration + 's'
+// 	}} className={className}>
+// 		<code>
+// 			{children}
+// 		</code>
+// 	</SxSC>
+// );
+
+const Pre = forwardRef<HTMLPreElement,
+	{children: ReactNode, className: string, duration: number}>
+	(({ children, className, duration }, ref) => {
+		return (
+			<SxSC ref={ref} as='pre' $sx={{
+				animationDuration: duration/2 + 's',
+				animationDelay: duration + 's'
+			}} className={className}>
+				<code>
+					{children}
+				</code>
+			</SxSC>
+		);
+	});
+
+Pre.displayName = 'Pre';
+
