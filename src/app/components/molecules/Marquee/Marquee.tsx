18cff9ea2260f0946baa91a0448879ab427fca6f
rime <s1337228@yandex.ru>
2023-12-22 17:53:27 +0300

    marquee minHeight inspection

--- src/app/components/molecules/Marquee/Marquee.tsx
+++ src/app/components/molecules/Marquee/Marquee.tsx
@@ -1,34 +1,16 @@
 'use client';
 
 import s from './Marquee.module.scss';
-import { forwardRef, ReactNode, useEffect, useRef } from 'react';
+import { ReactNode } from 'react';
 import clsx from 'clsx';
 import SxSC from '@/app/components/atoms/SxSC/SxSC';
 import { CSSProperties } from 'styled-components';
 
+/**
+ * You should use text with height >= marquee container, because it's unable to force minHeight due to background distortion
+ */
 const Marquee = ({ children, horizontal, sx, duration = 30 }:
 	{children: ReactNode, horizontal?: boolean, sx?: CSSProperties, duration?: number}) => {
-
-	const wrapperRef = useRef<HTMLDivElement>(null);
-	const firstPreRef = useRef<HTMLPreElement>(null);
-	const secondPreRef = useRef<HTMLPreElement>(null);
-
-	useEffect(() => {
-		const
-			wrapper = wrapperRef.current,
-			firstPre = firstPreRef.current,
-			secondPre = secondPreRef.current;
-		if (!wrapper || !firstPre || !secondPre) return;
-
-		const preHeight = firstPre.getBoundingClientRect().height;
-		const wrapperHeight = wrapper.getBoundingClientRect().height;
-
-		if (preHeight >= wrapperHeight) return;
-		const height = wrapperHeight + 'px';
-		firstPre.style.height = height;
-		secondPre.style.height = height;
-
-	}, []);
 
 	const classNameWrapper = clsx({
 		[s.codeElement]: true,
@@ -42,63 +24,28 @@ const Marquee = ({ children, horizontal, sx, duration = 30 }:
 		[s.animHorizontal]: horizontal
 	});
 
-	// const Pre =
-	// 	<SxSC as='pre' $sx={{
-	// 		animationDuration: duration/2 + 's',
-	// 		animationDelay: duration + 's'
-	// 	}} className={classNamePre}>
-	// 		<code>
-	// 			{children}
-	// 		</code>
-	// 	</SxSC>;
+	const pre =
+		<SxSC as='pre' $sx={{
+			animationDuration: duration/2 + 's',
+			animationDelay: duration + 's'
+		}} className={classNamePre}>
+			<code>
+				{children}
+			</code>
+		</SxSC>;
 
 	return (
-		<div className={s.marqueeWrapper} ref={wrapperRef}>
+		<div className={s.marqueeWrapper}>
 			<SxSC $sx={{
 				animationDuration: duration + 's',
 				...sx
 			}} className={classNameWrapper}>
-				{/*{Pre}*/}
-				{/*{Pre}*/}
-				<Pre ref={firstPreRef} className={classNamePre} duration={duration}>
-					{children}
-				</Pre>
-				<Pre ref={secondPreRef} className={classNamePre} duration={duration}>
-					{children}
-				</Pre>
+				{pre}
+				{pre}
 			</SxSC>
 		</div>
 	);
 };
 
 export default Marquee;
-
-// const Pre = ({ children, className, duration }:
-// 	{children: ReactNode, className: string, duration: number}) => (
-// 	<SxSC as='pre' $sx={{
-// 		animationDuration: duration/2 + 's',
-// 		animationDelay: duration + 's'
-// 	}} className={className}>
-// 		<code>
-// 			{children}
-// 		</code>
-// 	</SxSC>
-// );
-
-const Pre = forwardRef<HTMLPreElement,
-	{children: ReactNode, className: string, duration: number}>
-	(({ children, className, duration }, ref) => {
-		return (
-			<SxSC ref={ref} as='pre' $sx={{
-				animationDuration: duration/2 + 's',
-				animationDelay: duration + 's'
-			}} className={className}>
-				<code>
-					{children}
-				</code>
-			</SxSC>
-		);
-	});
-
-Pre.displayName = 'Pre';
 
