1ced5660142bc027e9e13ccd11351c63d1840d2e
rime <s1337228@yandex.ru>
2023-12-28 17:50:01 +0300

    BlueCross clean from LineBreakers

--- src/app/components/atoms/BlueCross/BlueCross.tsx
+++ src/app/components/atoms/BlueCross/BlueCross.tsx
@@ -5,11 +5,6 @@ import { animated, easings, useSpring } from '@react-spring/web';
 
 const size = 40;
 
-// const wh = {
-// 	width: 15,
-// 	height: 2
-// }
-
 const BlueCross = ({ delay = 0 }: {delay?: number}) => {
 	const [horizontalSprings] = useSpring(() => ({
 		from: {
@@ -38,62 +33,12 @@ const BlueCross = ({ delay = 0 }: {delay?: number}) => {
 
 	return (
 		<div className={s.wrapper}>
-			{/*<LineBreaker sx={{*/}
-			{/*	rotate: '-90deg',*/}
-			{/*	...wh,*/}
-			{/*	left: 10.5,*/}
-			{/*	top: 6.6*/}
-			{/*}} variant={LineBreakerVariant.startToEnd}/>*/}
-			{/*<LineBreaker sx={{*/}
-			{/*	rotate: '-90deg',*/}
-			{/*	...wh,*/}
-			{/*	left: 14.3,*/}
-			{/*	top: 6.6*/}
-			{/*}} variant={LineBreakerVariant.startToEnd} mirrored/>*/}
-			{/**/}
-			{/*<LineBreaker sx={{*/}
-			{/*	...wh,*/}
-			{/*	top: 16.9,*/}
-			{/*	left: 0.3*/}
-			{/*}} variant={LineBreakerVariant.startToEnd}/>*/}
-			{/*<LineBreaker sx={{*/}
-			{/*	...wh,*/}
-			{/*	top: 21,*/}
-			{/*	left: 0.3*/}
-			{/*}} variant={LineBreakerVariant.startToEnd} mirrored/>*/}
-			{/**/}
-			{/*<LineBreaker sx={{*/}
-			{/*	...wh,*/}
-			{/*	top: 16.9,*/}
-			{/*	left: 24.5*/}
-			{/*}} variant={LineBreakerVariant.startToEnd}/>*/}
-			{/*<LineBreaker sx={{*/}
-			{/*	...wh,*/}
-			{/*	top: 21,*/}
-			{/*	left: 24.5*/}
-			{/*}} variant={LineBreakerVariant.startToEnd} mirrored/>*/}
-			{/**/}
-			{/*<LineBreaker sx={{*/}
-			{/*	rotate: '-90deg',*/}
-			{/*	...wh,*/}
-			{/*	left: 10.5,*/}
-			{/*	top: 31.2*/}
-			{/*}} variant={LineBreakerVariant.startToEnd}/>*/}
-			{/*<LineBreaker sx={{*/}
-			{/*	rotate: '-90deg',*/}
-			{/*	...wh,*/}
-			{/*	left: 14.3,*/}
-			{/*	top: 31.2*/}
-			{/*}} variant={LineBreakerVariant.startToEnd} mirrored/>*/}
-
 			<animated.div style={{
 				width: 2,
-				// height: size,
 				...horizontalSprings
 			}} className={s.cross}/>
 
 			<animated.div style={{
-				// width: size,
 				height: 2,
 				...verticalSprings
 			}} className={s.cross}/>
