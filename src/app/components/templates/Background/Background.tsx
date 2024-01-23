9339caa801d1f3d1654df8a1c1ddb97f7ef84a18
rime <s1337228@yandex.ru>
2024-01-23 23:37:15 +0300

    new mobile background

--- src/app/components/templates/Background/Background.tsx
+++ src/app/components/templates/Background/Background.tsx
@@ -8,42 +8,48 @@ import TopTextLine from '@/app/components/molecules/TopTextLine/TopTextLine';
 import BackgroundBottom from '@/app/components/organisms/BackgroundBottom/BackgroundBottom';
 import RedCodeTopHelmets from '@/app/components/organisms/RedCodeTopHelmets/RedCodeTopHelmets';
 import BackgroundLighting from '@/app/components/organisms/BackgroundLighting/BackgroundLighting';
-import { mohave } from '@/app/theme';
+import { mohave, rajdhani } from '@/app/theme';
 import TopElement from '@/app/components/organisms/TopElement/TopElement';
 import { createClassName } from '@/app/components/shared/utils';
 import { useAnimationStore } from '@/app/components/shared/syncStore';
 import BlueCrossArray from '@/app/components/molecules/BlueCrossArray/BlueCrossArray';
 
+import digitalLine from './digital line.png';
+import Marquee from '@/app/components/molecules/Marquee/Marquee';
+import LoadingLog from '@/app/components/molecules/LoadingLog/LoadingLog';
+import Lamp from '@/app/components/atoms/Lamp/Lamp';
+
 const Background = ({ isMobile }: {isMobile: boolean}) => {
 	const preloaderVisibility = useAnimationStore(s => s.preloaderVisibility);
 
-	return (
-		<div className={createClassName(s.background, mohave)}>
-			<BackgroundLighting start={!preloaderVisibility} />
+	if (!isMobile)
+		return (
+			<div className={createClassName(s.background, mohave)}>
+				<BackgroundLighting start={!preloaderVisibility} />
 
-			{/*<PngLamp start={!preloaderVisibility} delay={600} duration={3000} cyan moreFalloff sx={{*/}
-			{/*	opacity: 0.2,*/}
-			{/*	height: 140,*/}
-			{/*	width: 3000,*/}
-			{/*	objectFit: 'fill',*/}
-			{/*	top: 0*/}
-			{/*}}/>*/}
-			{/*<PngLamp start={!preloaderVisibility} delay={200} duration={3000} moreFalloff sx={{*/}
-			{/*	opacity: 0.13,*/}
-			{/*	height: 130,*/}
-			{/*	width: 3000,*/}
-			{/*	objectFit: 'fill',*/}
-			{/*	top: 100*/}
-			{/*}}/>*/}
-			{/*<PngLamp start={!preloaderVisibility} delay={1500} duration={3000} sx={{*/}
-			{/*	opacity: 0.1*/}
-			{/*}}/>*/}
-			{/*<PngLamp start={!preloaderVisibility} delay={2000} duration={3000} cyan sx={{*/}
-			{/*	opacity: 0.11,*/}
-			{/*	top: 850,*/}
-			{/*}}/>*/}
+				{/*<PngLamp start={!preloaderVisibility} delay={600} duration={3000} cyan moreFalloff sx={{*/}
+				{/*	opacity: 0.2,*/}
+				{/*	height: 140,*/}
+				{/*	width: 3000,*/}
+				{/*	objectFit: 'fill',*/}
+				{/*	top: 0*/}
+				{/*}}/>*/}
+				{/*<PngLamp start={!preloaderVisibility} delay={200} duration={3000} moreFalloff sx={{*/}
+				{/*	opacity: 0.13,*/}
+				{/*	height: 130,*/}
+				{/*	width: 3000,*/}
+				{/*	objectFit: 'fill',*/}
+				{/*	top: 100*/}
+				{/*}}/>*/}
+				{/*<PngLamp start={!preloaderVisibility} delay={1500} duration={3000} sx={{*/}
+				{/*	opacity: 0.1*/}
+				{/*}}/>*/}
+				{/*<PngLamp start={!preloaderVisibility} delay={2000} duration={3000} cyan sx={{*/}
+				{/*	opacity: 0.11,*/}
+				{/*	top: 850,*/}
+				{/*}}/>*/}
 
-			{!preloaderVisibility &&
+				{!preloaderVisibility &&
 			<>
 				<TopElement />
 
@@ -58,9 +64,42 @@ const Background = ({ isMobile }: {isMobile: boolean}) => {
 
 				<BlueCrossArray quantity={30} additionalClassName={s.bottomCrosses}/>
 			</>
-			}
-		</div>
-	);
+				}
+			</div>
+		);
+
+	if (!preloaderVisibility)
+		return (
+			<div className={createClassName(s.mobBackground, rajdhani)}>
+				<div className={s.addLight}/>
+				<div className={s.loadingLogWrapper}>
+					<LoadingLog delay={1000}/>
+				</div>
+				<div className={s.digitalLineWrapper}>
+					<Marquee duration={20} delay={1}>
+						<div className={s.digitalLine} />
+						<div className={s.digitalLine} />
+					</Marquee>
+				</div>
+				<div className={s.powerElement}/>
+
+				<div className={s.taskManager}>
+					<div className={s.managerTab}/>
+					<p>
+						TRN_TLCAS_BD0095
+					</p>
+					<div className={s.wrappedLabel}>
+						<p>
+							TASK_MANAGER_S011s
+						</p>
+						<div className={s.tmTab} />
+						<p className={s.cadp}>
+							COMBAT ADAPTIVE DEFENCE PROGRAM
+						</p>
+					</div>
+				</div>
+			</div>
+		);
 };
 
 export default Background;
